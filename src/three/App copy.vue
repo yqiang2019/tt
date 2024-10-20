35e12ef93566b9414c96117f5b0f19d276116246
yqiang2019 <416239076@qq.com>
2024-10-20 21:56:24 +0800

    feat:three

---
+++ src/three/App copy.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import { TresCanvas } from "@tresjs/core";
+import { OrbitControls } from "@tresjs/cientos";
+</script>
+
+<template>
+  <TresCanvas window-size clear-color="#82DBC5">
+    <TresPerspectiveCamera :position="[9, 9, 9]" />
+    <OrbitControls />
+    <TresMesh :position="[-2, 2, 0]" :rotation="[0, Math.PI, 0]">
+      <TresConeGeometry :args="[1, 1.5, 3]" />
+      <TresMeshToonMaterial color="#82DBC5" />
+    </TresMesh>
+    <TresMesh :position="[0, 0, 0]" cast-shadow>
+      <TresBoxGeometry :args="[1.5, 1.5, 1.5]" />
+      <TresMeshToonMaterial color="#4F4F4F" />
+    </TresMesh>
+    <TresMesh :position="[2, -2, 0]">
+      <TresSphereGeometry />
+      <TresMeshToonMaterial color="#FBB03B" />
+    </TresMesh>
+    <TresDirectionalLight :position="[0, 2, 4]" :intensity="1.2" cast-shadow />
+  </TresCanvas>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+}
+</style>
---
+++ src/three/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+#app {
+  height: 100%;
+  width: 100%;
+  background-color: #000;
+}
+</style>
